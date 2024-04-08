6e487f04f9b9de9a1cad51b5b36c71270ce62b58
Igivotron <gregoire.igor@gmail.com>
2024-04-08 17:44:27 +0200

    speed test

---
+++ le_temps_des_cerises.py
@@ -0,0 +1,38 @@
+import numpy as np
+from time import time
+import matplotlib.pyplot as plt
+from main import resolution
+from extract_data import ExtractData
+
+sites_file = "Data-partie-1/Sites.csv"
+onshore_file = "Data-partie-1/Rendements_onshore.csv"
+offshore_file = "Data-partie-1/Rendements_offshore.csv"
+
+def resolution_speed(sites_file, onshore_file, offshore_file, timey, episode):
+    """
+    :param sites_file: sites file data
+    :param onshore_file: onshore rendements data
+    :param offshore_file: offshore rendements data
+    :param timey: how many times to execute the subsimulation
+    :param episode: number of pool to test
+    """
+    m = int(642/episode)
+    registre_temporel = np.zeros(episode+1)
+    registre_n = np.zeros(episode+1)
+    for i in range(1, episode+1):
+        data = ExtractData(sites_file, onshore_file, offshore_file, n=i*m)
+        time_list = np.zeros(timey)
+        for j in range(timey):
+            start_time = time()
+            resolution(data)
+            end_time = time()
+            time_list[j] = end_time - start_time
+        registre_temporel[i] = np.average(time_list)
+        registre_n[i] = i*m
+    return registre_n, registre_temporel
+
+n, speed = resolution_speed(sites_file, onshore_file, offshore_file, 10, 25)
+print(len(n))
+plt.plot(n, speed)
+plt.grid(which='both')
+plt.show()
