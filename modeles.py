fa8c29ca7328afb211e72b119cb0d0854d84e4a9
Igivotron <gregoire.igor@gmail.com>
2024-04-12 19:18:54 +0200

    V1

--- modeles.py
+++ modeles.py
@@ -55,9 +55,10 @@ def modele1 (t, m, n, h, ct, cm, c, Rt, Rm, R, A, P, k, checkpoints) :
     A_eq2 = np.concatenate((np.zeros((t)), cm))
     A_eq2 = np.concatenate((A_eq2, np.zeros((1))))
     A_eq = np.array([A_eq1, A_eq2])
+
     if checkpoints :
         print("Checking construction of A_eq : is A_eq shape correct ?", np.shape(A_eq)==(2,n+1))
-    
+
     coeffs = np.concatenate((np.zeros((n)), np.array([-1])))
     if checkpoints :
         print("Checking construction of coeffs : is coeffs shape correct ?", np.shape(coeffs)==(n+1,))
@@ -101,7 +102,7 @@ def mod3ContSup(R, c, T):
     # On coupe la matrice car sinon il n'y a pas assez de données de remplir la dernière période
     if r != 0:
         A = A[:, :-r]
-    # C :matrice [p x p*T] pour additionner les productions d'une période
+    # C :matrice pour additionner les productions d'une période
     C = np.zeros((A.shape[1], p))
     for i in range(p):
         C[i * T:(i + 1) * T, i] = 1
@@ -109,12 +110,15 @@ def mod3ContSup(R, c, T):
     P = np.dot(A, C)
     # B : matrice pour calculer les différences de productions entre période successive
     B = np.eye(p, p - 1, k=-1) - np.eye(p, p - 1)
-    # D: matrice des deltaP
-    D = np.abs(np.dot(P, B))
-    # On addictionne tous les deltaP :
-    S = np.ones((D.shape[1]))
-    F = np.dot(D,S)/(p-1)
-    return F
+    # D : matrice des deltaP
+    D = np.dot(P, B)
+    # E : matrice pour faire la différence entre colonnes successives
+    E = np.eye(D.shape[1], D.shape[0]+1, k=-1) - np.eye(D.shape[1], D.shape[0]+1)
+    # F : matrice Ak+1 - Ak
+    F = np.dot(D, E)
+    # G : matrice Ak - Ak+1
+    G = np.dot(D, -E)
+    return F.T, G.T
 
 def modele3(t, m, n, h, ct, cm, c, Rt, Rm, R, A, P, k, delta, T, checkpoints):
     """
@@ -129,7 +133,7 @@ def modele3(t, m, n, h, ct, cm, c, Rt, Rm, R, A, P, k, delta, T, checkpoints):
 
     """
     ## Constructing all the matrices of use for linprog
-    ## v are the variables linprog will give us in return, v = (x|z)
+    ## v are the variables linprog will give us in return, v = (x|d|z)
     ## z is the optimized objective linprog will give us in return
     ## A_ub @ v <= b_ub
     ## A_eq @ v == b_eq
@@ -144,41 +148,34 @@ def modele3(t, m, n, h, ct, cm, c, Rt, Rm, R, A, P, k, delta, T, checkpoints):
 
     In = np.identity(n)
     A_ub1 = np.concatenate((In, -In))
-    if checkpoints:
-        print("Checking construction of A_ub : for now, is A_ub1 shape correct ?", np.shape(A_ub1) == (2 * n, n))
-    A_ub1 = np.concatenate((A_ub1, np.zeros((2 * n))[:, np.newaxis]), axis=1)
-    if checkpoints:
-        print("Checking construction of A_ub : is A_ub1 shape correct ?", np.shape(A_ub1) == (2 * n, n + 1))
-    A_ub2 = np.concatenate((-np.transpose(A), np.ones((h))[:, np.newaxis]), axis=1)
-    if checkpoints:
-        print("Checking construction of A_ub : is A_ub2 shape correct ?", np.shape(A_ub2) == (h, n + 1))
-    A_ub3 = np.array([np.append(mod3ContSup(R, c, T), 0)])
-    print(A_ub3)
-    A_ub = np.concatenate((A_ub1, A_ub2, A_ub3))
-    if checkpoints:
-        print("Checking construction of A_ub : is A_ub shape correct ?", np.shape(A_ub) == (2 * n + h, n + 1))
+    F, G = mod3ContSup(R, c, T)
+    m = F.shape[0] #p-1
+
+    A_ub1 = np.concatenate((A_ub1, np.zeros((A_ub1.shape[0], m)), np.zeros((2 * n))[:, np.newaxis]), axis=1)
+    A_ub2 = np.concatenate((-np.transpose(A), np.zeros((A.shape[1], m)),np.ones((h))[:, np.newaxis]), axis=1)
+    A_ub3 = np.concatenate((F,-np.identity(F.shape[0]),np.zeros((m,1))), axis=1)
+    A_ub4 = np.concatenate((G,-np.identity(G.shape[0]),np.zeros((G.shape[0],1))), axis=1)
+    A_ub5 = np.concatenate((np.zeros((1,n)), np.ones((1,m)), np.zeros((1,1))), axis=1)
+    A_ub = np.concatenate((A_ub1, A_ub2, A_ub3, A_ub4, A_ub5))
+    print(A_ub.shape)
+
     b_ub1 = np.ones((n))
     b_ub2 = np.zeros((n+h))
-    b_ub3 = np.array([delta*P*T])
-    b_ub = np.concatenate((b_ub1, b_ub2, b_ub3))
-    if checkpoints:
-        print("Checking construction of b_ub : is b_ub shape correct ?", np.shape(b_ub) == (2 * n + h,))
+    b_ub3 = np.zeros((2*m))
+    b_ub4 = np.array([delta*T*P*m])
+    b_ub = np.concatenate((b_ub1, b_ub2, b_ub3, b_ub4))
 
     # Contraintes d'égalité (A_eq, b_eq)
     # c @ x = P
     # cm @ xm = kP
 
     b_eq = np.array([P, k * P])
-    A_eq1 = np.concatenate((c, np.zeros((1))))
-    A_eq2 = np.concatenate((np.zeros((t)), cm))
-    A_eq2 = np.concatenate((A_eq2, np.zeros((1))))
+    A_eq1 = np.concatenate((c, np.zeros((m)), np.zeros((1))))
+    A_eq2 = np.concatenate((np.zeros((t)), cm, np.zeros((m)),np.zeros((1))))
     A_eq = np.array([A_eq1, A_eq2])
-    if checkpoints:
-        print("Checking construction of A_eq : is A_eq shape correct ?", np.shape(A_eq) == (2, n + 1))
 
-    coeffs = np.concatenate((np.zeros((n)), np.array([-1])))
-    if checkpoints:
-        print("Checking construction of coeffs : is coeffs shape correct ?", np.shape(coeffs) == (n + 1,))
+    coeffs = np.concatenate((np.zeros((n)), np.zeros((m)), np.array([-1])))
+
 
     ## Calling linprog
     res = linprog(coeffs, A_ub, b_ub, A_eq, b_eq)
