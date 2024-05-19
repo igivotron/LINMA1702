2e5789c71f4073823a6b7be8a5adedd4dbcd1288
bolclaire <claire.boland.gilon@gmail.com>
2024-05-19 16:36:11 +0200

    Prix moyens et temps

--- P2/results.py
+++ P2/results.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def print_values(prix_total, e_eolienne_produite, apports_naturels, demande_totale, e_gaz_produite=0) :
+def print_values_get_prixmoy(prix_total, e_eolienne_produite, apports_naturels, demande_totale, e_gaz_produite=0) :
 
     prix_moyen = prix_total/(e_eolienne_produite + apports_naturels + e_gaz_produite)
     perte = e_eolienne_produite + apports_naturels + e_gaz_produite - demande_totale
@@ -14,25 +14,28 @@ def print_values(prix_total, e_eolienne_produite, apports_naturels, demande_tota
     print("\n")
     print("Prix total, {:.2f} euros".format(prix_total))
     print("Prix moyen, {:.2f} euros/MWh".format(prix_moyen))
+    return prix_moyen
 
 
-def occupation_bassin(t, s, wA, wP, sorties) :
+def occupation_bassin(t, s, wA, wP, wT) :
 
     plt.figure()
 
     L = np.tril(np.ones((t,t)))
+    nticks = 100
+    step=min(nticks,t)
 
     time = np.arange(0,t,1)
 
-    min = -0.5*s*np.ones((t))
-    max = 0.5*s*np.ones((t))
+    Wmin = -0.5*s*np.ones((t))
+    Wmax = 0.5*s*np.ones((t))
 
-    plt.plot(time, min, 'r', label="Limites de stockage")
-    plt.plot(time, max, 'r')
+    plt.plot(time, Wmin, 'r', label="Limites de stockage")
+    plt.plot(time, Wmax, 'r')
     plt.plot(time, L@wA, 'b', label="Apports naturels cumulés")
     plt.plot(time, L@wP, 'g', label="Entrées cumulées")
-    plt.plot(time, -L@sorties, 'y', label="Sorties cumulées")
-    plt.plot(time, L@(wA + wP - sorties), 'black', label="Occupation du bassin")
+    plt.plot(time, -L@wT, 'y', label="Sorties cumulées")
+    plt.plot(time, L@(wA + wP - wT), 'black', label="Occupation du bassin")
 
     plt.xlabel("Nombre de periodes [-]")
     plt.ylabel("Energie stockée [MWh]")
@@ -41,7 +44,7 @@ def occupation_bassin(t, s, wA, wP, sorties) :
     plt.show()
 
 
-def sources_eng(t, e, wP, wT, d, epg=None) :
+def sources_eng(t, e, wP, wT_dim, d, epg=None) :
 
     try :
         if (epg==None) :
@@ -49,17 +52,35 @@ def sources_eng(t, e, wP, wT, d, epg=None) :
     except :
         pass
 
+    L = np.tril(np.ones((t,t)))
+
     plt.figure()
 
     time = np.arange(0,t,1)
     plt.plot(time, e, 'r', label="Energie éolienne produite")
     plt.plot(time, -wP, 'g', label="Energie pompée (stockée)")
-    plt.plot(time, wT, 'y', label="Energie hydroélectrique turbinée")
+    plt.plot(time, wT_dim, 'y', label="Energie hydroélectrique turbinée")
     plt.plot(time, epg, 'black', label="Energie au gaz produite")
-    plt.plot(time, e - wP + wT + epg, 'b', label="Energie employée")
+    plt.plot(time, e - wP + wT_dim + epg, 'b', label="Energie employée")
     plt.plot(time, d, '--b', label="Demande")
 
     plt.xlabel("Nombre de periodes [-]")
     plt.ylabel("Energie produite [MWh]")
     plt.legend()
     plt.plot()
+    
+    plt.figure()
+
+    time = np.arange(0,t,1)
+    plt.plot(time, L@e, 'r', label="Energie éolienne produite")
+    plt.plot(time, -L@wP, 'g', label="Energie pompée (stockée)")
+    plt.plot(time, L@wT_dim, 'y', label="Energie hydroélectrique turbinée")
+    plt.plot(time, L@epg, 'black', label="Energie au gaz produite")
+    plt.plot(time, L@(e - wP + wT_dim + epg), 'b', label="Energie employée")
+    plt.plot(time, L@d, '--b', label="Demande")
+
+    plt.xlabel("Nombre de periodes [-]")
+    plt.ylabel("Energie produite cumulée [MWh]")
+    plt.legend()
+    plt.plot()
+
